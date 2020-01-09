eefa10eacfcefae46537f53b2f0453ba7168cead
JiahaoWu <jiahaowu@boyaa.com>
2020-01-09 15:21:25 +0800

    03. 添加玩家网络同步操作

---
+++ Assets/Editor/ScriptsInfoRecoder.cs
@@ -0,0 +1,23 @@
+﻿/************************************************************
+    文件: ScriptsInfoRecoder.cs
+	作者：JiahaoWu
+    邮箱: jiahaodev@163.com
+    日期: 2013/10/13 12:01
+	功能: 记录脚本信息
+*************************************************************/
+
+using System;
+using System.IO;
+
+public class ScriptsInfoRecoder : UnityEditor.AssetModificationProcessor {
+    private static void OnWillCreateAsset(string path) {
+        path = path.Replace(".meta", "");
+        if (path.EndsWith(".cs")) {
+            string str = File.ReadAllText(path);
+            str = str.Replace("#CreateAuthor#", Environment.UserName)
+                     .Replace("#CreateTime#", DateTime.Now.ToString("yyyy/MM/dd HH:mm"));
+
+            File.WriteAllText(path, str);
+        }
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿/****************************************************
+    文件：PlayerController.cs
+    作者：JiahaoWu
+    邮箱: jiahaodev@163.ccom
+    日期：2020/01/09 15:00       
+    功能：玩家控制脚本
+*****************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public float rotSpeed = 150.0f;
+    public float movSpeed = 3.0f;
+
+    private float x, z;
+
+    private void Update()
+    {
+        //判断是否为本地玩家
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        x = Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed;
+        z = Input.GetAxis("Vertical") * Time.deltaTime * movSpeed;
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
