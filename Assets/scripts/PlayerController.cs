4e0f396bc1879e4b503b32a4af5e3972b30da2d1
JiahaoWu <jiahaowu@boyaa.com>
2020-01-09 15:45:58 +0800

    04.添加子弹预制体

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -14,6 +14,8 @@ public class PlayerController : NetworkBehaviour
 {
     public float rotSpeed = 150.0f;
     public float movSpeed = 3.0f;
+    public GameObject bulletPrefab; //子弹预制体
+    public Transform bulletSpawn;  //子弹生成位置
 
     private float x, z;
 
@@ -25,13 +27,34 @@ public class PlayerController : NetworkBehaviour
             return;
         }
         PlayerMove();
+        Fire();
     }
 
+    //玩家移动检测
     private void PlayerMove()
     {
         x = Input.GetAxis("Horizontal") * Time.deltaTime * rotSpeed;
         z = Input.GetAxis("Vertical") * Time.deltaTime * movSpeed;
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+    }
+
+    //玩家开火检测
+    private void Fire() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //生成子弹
+            var bullet = Instantiate(bulletPrefab,bulletSpawn.position,bulletSpawn.rotation);
+            //给子弹加速度
+            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+            //销毁子弹
+            Destroy(bullet,2.0f);
+        }
+    }
+
+    //NetworkBehaviour接口重写
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
     }
 }
